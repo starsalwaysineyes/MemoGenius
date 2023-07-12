17eb8c55a0e7a87821d72aa6ba5ff0347d928192
starsalwaysineyes <114756180+starsalwaysineyes@users.noreply.github.com>
2023-07-12 12:20:47 +0000

    新增文件：imports

---
+++ code/客户端/imports.py
@@ -0,0 +1,14 @@
+import pyaudio
+import wave
+import threading
+import time
+
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageDraw, ImageTk
+import ctypes
+import os
+import threading
+import time
+
+import func_phpUpload
